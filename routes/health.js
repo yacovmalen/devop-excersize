a57bee93c0e5cd409b7fe9f69bd8d6c7a6f7597f
shaharke <shahar@bigpanda.io>
2018-05-13 12:01:28 +0300

    Adds health check route

--- routes/health.js
+++ routes/health.js
@@ -1,9 +1,46 @@
+const Promise = require('bluebird');
 const express = require('express');
 const router = express.Router();
 
+
+function checkDatabase() {
+  return Promise.resolve();
+}
+
+function checkDisk() {
+  return Promise.resolve();
+}
+
+function healthCheck() {
+  const checks = [checkDatabase, checkDisk]
+  return Promise.reduce(checks, (result, check) => {
+    return check().then((checkResult) => {
+      result[check.name] = {
+        failed: false,
+        result: checkResult
+      }
+      return result;
+    }, (error) => {
+      result[check.name] = {
+        failed: false,
+        result: error.message
+      }
+      result.isSuccessful = false
+      return result;
+    })
+  }, { isSuccessful: true} )
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendStatus(200);
+  console.log('test');
+  healthCheck().then((result) => {
+    const status = result.isSuccessful === false ? 500 : 200;
+    res.status(status).send(result);
+  }, (err) => {
+    res.status(500).send( { isSuccessful: false, error: err.message});
+  })
+  
 });
 
 module.exports = router;
